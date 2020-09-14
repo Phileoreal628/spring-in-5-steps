1e882cde5dbf1a2c91032b29847b08494c8c4083
chiraggupta628 <chiraggupta629@gmail.com>
2020-09-14 18:23:25 +0530

    added

--- src/main/java/com/In28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/In28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -3,11 +3,16 @@ package com.In28minutes.spring.basics.springin5steps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIn5StepsApplication.class, args);
+
+		System.out.println("Hello");
 	}
 
 }
